cecb84795cb51c7b54cafee364a13e49794223c3
theajack <1506282385@qq.com>
2018-03-15 14:00:39 +0800

    1

--- src/js/count_p_top.js
+++
@@ -1,7 +0,0 @@
-(function(){
-    var height=(document.documentElement.clientHeight-556)/2;
-    var style=document.createElement('style');
-    style.innerHTML='body{padding-top:'+height+'px}'
-    var head=document.getElementsByTagName('head')[0];
-    head.appendChild(style);
-})()
